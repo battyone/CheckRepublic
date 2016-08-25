3c98f693ef6ede6646036fb0e7e400f73721fb3c
Joel Verhagen <jver@microsoft.com>
2016-08-24 17:51:30 -0700

    Initial commit

---
+++ src/CheckRepublic.Sandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("CheckRepublic.Sandbox")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8f6073f7-e653-418a-817b-a3a33d1f6ead")]
